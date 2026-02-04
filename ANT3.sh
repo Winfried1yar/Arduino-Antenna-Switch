cdfd382559e4a7ae8dfec0e7cc6db255d5cc7688
Winfried DL1YAR <33581794+Winfried1yar@users.noreply.github.com>
2026-02-04 07:00:28 +0100

    Add files via upload

---
+++ ANT3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+stty -F /dev/Arduino 9600 -parenb -parodd -cmspar cs8 hupcl -cstopb cread clocal -crtscts -ignbrk -brkint -ignpar -parmrk -inpck -istrip -inlcr -igncr -icrnl -ixon -ixoff -iuclc -ixany -imaxbel -iutf8 -opost -olcuc -ocrnl onlcr -onocr -onlret -ofill -ofdel nl0 cr0 tab0 bs0 vt0 ff0 -isig -icanon -iexten -echo -echoe -echok -echonl -noflsh -xcase -tostop -echoprt -echoctl -echoke -flusho -extproc
+echo -E Ant3 > /dev/Arduino
+cat /dev/Arduino |tee -a >> /dev/null
+
